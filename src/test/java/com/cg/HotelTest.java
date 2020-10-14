90e846e41ef0b12edcaf7e2f880d79b09ca747df
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-14 20:12:15 +0530

    [Arijit]Modified : Cheapest Hotel With Different Weekday And Weekend Rates

--- src/test/java/com/cg/HotelTest.java
+++ src/test/java/com/cg/HotelTest.java
@@ -9,6 +9,9 @@ import junit.framework.Assert;
 import static org.junit.Assert.*;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HotelTest {
     
@@ -17,5 +20,13 @@ public class HotelTest {
 		HotelReservationMain hotelReservationMain = new HotelReservationMain();
 		String hotel = hotelReservationMain.findCheapestHotelForRegularCustomersInWeekDayRate("10Sep2020","11Sep2020");
 	    Assert.assertEquals("LakeWood", hotel);
+	}
+	
+	@Test
+	public void checkCheapestHotelForRegularCustomerConsideringWeekdayAndWeekend() throws ParseException {
+		HotelReservationMain hotelReservationMain = new HotelReservationMain();
+		List<String> hotelList = hotelReservationMain.findCheapestHotelForRegularCustomersConsideringWeekdayAndWeekend("10Sep2020","11Sep2020");
+	    List<String> expectedList = new ArrayList<>(Arrays.asList("LakeWood","BridgeWood"));
+	    Assert.assertEquals(expectedList, hotelList);
 	}
 }
