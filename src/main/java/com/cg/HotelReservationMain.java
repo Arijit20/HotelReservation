cfae469d3e9a9b7deebf88895620c39683766bc7
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-14 19:29:07 +0530

    [Arijit]Added : Added Hotels In System

--- src/main/java/com/cg/HotelReservationMain.java
+++ src/main/java/com/cg/HotelReservationMain.java
@@ -1,7 +1,18 @@
 package com.cg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelReservationMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Hotel Reservation Page");
+	final Hotel LAKEWOOD = new Hotel("LakeWood", 110);
+	final Hotel BRIDGEWOOD = new Hotel("BridgeWood", 150);
+	final Hotel RIDGEWOOD = new Hotel("RidgeWood", 220);
+
+	List<Hotel> hotelList = new ArrayList<>();
+
+	public HotelReservationMain() {
+		hotelList.add(LAKEWOOD);
+		hotelList.add(BRIDGEWOOD);
+		hotelList.add(RIDGEWOOD);
 	}
 }
